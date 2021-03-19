38a9d4324856aa98c7efe1c69a455096da73b7d5
Susanne Heinrichs <s6suhein@uni-bonn.de>
2021-03-19 16:25:07 +0100

    implement first steps of snake game: - snake segments follow each other in same order - snake orientation controlled by key input

---
+++ Day20+21/Snake/snake.py
@@ -0,0 +1,51 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.snake_parts = []
+        self.create_snake()
+        self.head = self.snake_parts[0]
+
+    def create_snake(self):
+        for pos in STARTING_POSITIONS:
+            snake_part = Turtle(shape="square")
+            snake_part.penup()
+            snake_part.color('white')
+            snake_part.goto(pos)
+            self.snake_parts.append(snake_part)
+
+    def move(self):
+        for part_num in range(len(self.snake_parts) - 1, 0, -1):
+            new_x = self.snake_parts[part_num - 1].xcor()
+            new_y = self.snake_parts[part_num - 1].ycor()
+            self.snake_parts[part_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+
+
+
+
